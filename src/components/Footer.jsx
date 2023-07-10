baff2c9f898cae57bb439953fedd81a0754a54e2
Lakshya1233 <109671929+Lakshya1233@users.noreply.github.com>
2023-07-10 09:00:31 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ LAKSHYA BHARDWAJ{year}</p>
     </footer>
   );
 }
